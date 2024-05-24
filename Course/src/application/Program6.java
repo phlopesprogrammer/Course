f8311170a72b3f4d7ad73dad6afe11c70f376093
phlopesprogrammer <phlopesprogrammer@gmail.com>
2024-05-24 16:30:58 -0300

    Program6 modelo3 - Classe Calculator com método estático

--- Course/src/application/Program6.java
+++ Course/src/application/Program6.java
@@ -17,15 +17,14 @@ public class Program6 {
 
 		System.out.println("Enter radius: ");
 		double radius = sc.nextDouble();
-		Calculator calc = new Calculator();
 
-		double c = calc.circunferemce(radius);
-		double v = calc.volume(radius);
+		double c = Calculator.circunferemce(radius);
+		double v = Calculator.volume(radius);
 
 		System.out.printf("Circunference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
 		
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 
 		sc.close();
 
