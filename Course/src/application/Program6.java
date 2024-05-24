b6c1c6c5fcd502f3b3e568f6814a35c73ca79984
phlopesprogrammer <phlopesprogrammer@gmail.com>
2024-05-24 14:19:27 -0300

    teste

--- Course/src/application/Program6.java
+++ Course/src/application/Program6.java
@@ -24,6 +24,7 @@ public class Program6 {
 
 		System.out.printf("Circunference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
+		
 		System.out.printf("PI value: %.2f%n", calc.PI);
 
 		sc.close();
