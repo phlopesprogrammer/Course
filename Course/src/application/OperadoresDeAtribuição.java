74ad1fdec5d9fbc233c5958b8e0c7f2d48a96f11
Paulo Henrique <phlopesprogrammer@gmail.com>
2025-12-24 18:02:53 -0300

    Operadores de atribuição

---
+++ Course/src/application/OperadoresDeAtribuição.java
@@ -0,0 +1,17 @@
+package application;
+
+public class OperadoresDeAtribuição {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 5;
+		numero += 3;
+		numero -= 2;
+		numero *= 4;
+		numero /= 2;
+		
+		System.out.println(" Valor Final:" + numero);
+	}
+
+}
