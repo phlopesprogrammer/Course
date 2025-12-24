3137f02bc30ede0e5cc9f9d416f8e38cfb163204
Paulo Henrique <phlopesprogrammer@gmail.com>
2025-12-24 17:44:12 -0300

    Merge branch 'master' of https://github.com/phlopesprogrammer/Course.git

---
+++ Course/src/application/CalculadoraSimples.java
@@ -0,0 +1,23 @@
+package application;
+
+public class CalculadoraSimples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 2 ;
+		int soma = a + b;
+		int subtracao = a - b;
+		int multiplicacao = a * b;
+		int divisao = a / b;
+		int resto = a % b;
+		
+		System.out.println("O valor da soma é : " + soma);
+		System.out.println("O valor da subtração é : " + subtracao);
+		System.out.println("O valor da multiplicação é : " + multiplicacao);
+		System.out.println("O valor da divisão é : " + divisao);
+		System.out.println("O valor da resto é : " + resto);
+	}
+
+}
